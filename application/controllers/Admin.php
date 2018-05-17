1ce6e577c46a5978d99ba877d5a4949eee07028d
ahiyalala <llaneratizam@gmail.com>
2018-05-17 22:07:53 +0800

    Revert "Admin breather"

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -34,6 +34,7 @@ class Admin extends CI_Controller {
     public function employee($id=null){
         $this->load->helper('form');
         $this->load->model('housekeeper');
+        
         $data = array(
             'admin' => array(
                 'first_name' => "Kuroneko",
@@ -47,13 +48,6 @@ class Admin extends CI_Controller {
             $view = 'admin-5-employee-profile';
         }
         else{
-            $query = 'select h.first_name as h_first_name, h.last_name as h_last_name, c.first_name as c_first_name, c.last_name as c_last_name, hs.date, hs.start_time, hs.end_time
-                      from housekeeper_schedule as hs
-                      left join housekeeper as h on h.housekeeper_id=hs.housekeeper_id
-                      left join booking_request as b on b.schedule_id=hs.schedule_id
-                      left join customer as c on c.customer_id=b.customer_id
-                      where hs.availability <> 0';
-            $data['housekeeper_schedules'] = $this->db->query($query)->result();
             $data['housekeepers'] = $this->housekeeper->get_all();
             $view = 'admin-4-employee';
         }
