531aa84bbf91146bb3fc1a591638b62808d51073
FGTG <gella.francis@gmail.com>
2018-06-23 20:53:02 +0800

    try again

--- application/views/index.php
+++ application/views/index.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>EZWelcome to Green Clean!</title>
+    <title>Welcome to Green Clean!</title>
     <link rel="stylesheet" href="css/bootstrap.min.css">
     <link rel="stylesheet" href="fonts/font-awesome.min.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Cookie">
