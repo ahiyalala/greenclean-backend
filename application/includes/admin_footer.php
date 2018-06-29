144e6227306ce5abe8208a6d343b3294ca631d58
FGTG <gella.francis@gmail.com>
2018-06-29 18:41:54 +0800

    Try again

--- application/includes/admin_footer.php
+++ application/includes/admin_footer.php
@@ -35,7 +35,9 @@
    <script src="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.18.1/moment.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.4.0/fullcalendar.min.js"></script> 
-
+   <script src="<?php echo base_url("/front/js/table.js");?>"></script>
+-  <script src="<?php echo base_url("/front/js/sidebar.js");?>"></script>
+-  <script src="<?php echo base_url("front/js/calendar.js");?>"></script>
   <script src="front/js/table.js"></script>
   <script src="front/js/calendar.js"></script>
   <script src="front/js/sidebar.js"></script>
