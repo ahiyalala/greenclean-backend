207c7f0aaf5206410bd97a8fd9ad829bd5386f25
FGTG <gella.francis@gmail.com>
2018-06-29 17:43:58 +0800

    fix

--- application/includes/admin_footer.php
+++ application/includes/admin_footer.php
@@ -18,7 +18,7 @@
   <!-- Bootstrap core JavaScript -->
   <!--<script src="<?php echo base_url("assets/js/jquery.min.js");?>"></script>
   <script src="assets/js/jquery.min.js"></script>-->
-  <script src="front/js/jquery.min.js"></script>
+  <script src="../../front/js/jquery.min.js"></script>
   <script src="https://code.jquery.com/jquery-3.2.1.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4="
     crossorigin="anonymous"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.js"></script>
@@ -35,9 +35,9 @@
    <script src="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.18.1/moment.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.4.0/fullcalendar.min.js"></script> 
-  <script src="front/js/table.js"></script>
-  <script src="front/js/calendar.js"></script>
-  <script src="front/js/sidebar.js"></script>
+  <script src="../../front/js/table.js"></script>
+  <script src="../../front/js/calendar.js"></script>
+  <script src="../../front/js/sidebar.js"></script>
   
 
   
