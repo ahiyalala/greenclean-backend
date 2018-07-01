0ed1554f659563da70046a3cc905c6fa48319a98
FGTG <gella.francis@gmail.com>
2018-07-01 16:57:55 +0800

    edit again

--- front/js/sidebar.js
+++ front/js/sidebar.js
@@ -3,28 +3,28 @@
 //   $("#wrapper").toggleClass("toggled");
 // });
 
-// let side ="close"; 
+let side ="close"; 
 // jQuery('#menu-toggle').bind('click mouseover', function (e) {
   
-//   e.preventDefault();
-//       $("#wrapper").toggleClass("toggled");
-//       if (side == "close"){
-//         return side = "open";
-//       }else{
-//         return side = "close"; 
-//       }
+  e.preventDefault();
+      $("#wrapper").toggleClass("toggled");
+      if (side == "close"){
+        return side = "open";
+      }else{
+        return side = "close"; 
+      }
 // });
 
-// $(window).scroll(function(e){
-//       if ($(document).scrollTop() > 0 && side == "open"){
-//         side ="close"
-//       e.preventDefault();
-//       $("#wrapper").toggleClass("toggled");
+$(window).scroll(function(e){
+      if ($(document).scrollTop() > 0 && side == "open"){
+        side ="close"
+      e.preventDefault();
+      $("#wrapper").toggleClass("toggled");
       
-//       }
-// });
-// window.setTimeout(function() {
-//     $(".alert").fadeTo(500, 0).slideUp(500, function(){
-//         $(this).remove(); 
-//     });
-// }, 4000);
+      }
+});
+window.setTimeout(function() {
+    $(".alert").fadeTo(500, 0).slideUp(500, function(){
+        $(this).remove(); 
+    });
+}, 4000);
