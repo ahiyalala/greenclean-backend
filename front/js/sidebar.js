3d2d51f8c8d4bf9768c044b6849582b51ff6a3f3
FGTG <gella.francis@gmail.com>
2018-07-01 19:18:43 +0800

    sidebar

--- front/js/sidebar.js
+++ front/js/sidebar.js
@@ -32,10 +32,11 @@
  let side ="close"; 
  let number = 0;
  console.log(side + ": " + ++number);
+
 $(window).scroll(function(e){
       if ($(document).scrollTop() > 0 && side == "open"){
         console.log(side + ": " + ++number);
-        return side ="close"
+        side ="close"
 
       e.preventDefault();
       $("#wrapper").toggleClass("toggled");
@@ -46,11 +47,19 @@ $(window).scroll(function(e){
 
 
 $("#menu-toggle").click(function (e) {
-  return side ="open"; 
-  console.log(side + ": " + ++number);
+   if (side == "close"){
+    side ="open"; 
+   }else{
+     side ="close";
+   }
+
+   
+   console.log(side + ": " + ++number);
   e.preventDefault();
   $("#wrapper").toggleClass("toggled");
 });
+
+
 window.setTimeout(function() {
 $(".alert").fadeTo(500, 0).slideUp(500, function(){
     $(this).remove(); 
