3e466f4de20d52a1a1618a6c9d93038da670574b
FGTG <gella.francis@gmail.com>
2018-07-01 18:10:53 +0800

    sidebar back to normal

--- front/js/sidebar.js
+++ front/js/sidebar.js
@@ -1,30 +1,40 @@
+// $("#menu-toggle").click(function (e) {
+//   e.preventDefault();
+//   $("#wrapper").toggleClass("toggled");
+// });
+
+// let side ="close"; 
+// jQuery('#menu-toggle').bind('click mouseover', function (e) {
+  
+//   e.preventDefault();
+//       $("#wrapper").toggleClass("toggled");
+//       if (side == "close"){
+//         return side = "open";
+//       }else{
+//         return side = "close"; 
+//       }
+// });
+
+// $(window).scroll(function(e){
+//       if ($(document).scrollTop() > 0 && side == "open"){
+//         side ="close"
+//       e.preventDefault();
+//       $("#wrapper").toggleClass("toggled");
+      
+//       }
+// });
+// window.setTimeout(function() {
+//     $(".alert").fadeTo(500, 0).slideUp(500, function(){
+//         $(this).remove(); 
+//     });
+// }, 4000);
+
 $("#menu-toggle").click(function (e) {
   e.preventDefault();
   $("#wrapper").toggleClass("toggled");
 });
-
-let side ="close"; 
-jQuery('#menu-toggle').bind('click mouseover', function (e) {
-  
-  e.preventDefault();
-      $("#wrapper").toggleClass("toggled");
-      if (side == "close"){
-        return side = "open";
-      }else{
-        return side = "close"; 
-      }
-});
-
-$(window).scroll(function(e){
-      if ($(document).scrollTop() > 0 && side == "open"){
-        side ="close"
-      e.preventDefault();
-      $("#wrapper").toggleClass("toggled");
-      
-      }
-});
 window.setTimeout(function() {
-    $(".alert").fadeTo(500, 0).slideUp(500, function(){
-        $(this).remove(); 
-    });
+$(".alert").fadeTo(500, 0).slideUp(500, function(){
+    $(this).remove(); 
+});
 }, 4000);
