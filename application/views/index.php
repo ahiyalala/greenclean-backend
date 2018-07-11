e1d93e560835dc56ea201c870de319dbf5ee60c2
FGTG <gella.francis@gmail.com>
2018-07-11 22:52:32 +0800

    FAQ

--- application/views/index.php
+++ application/views/index.php
@@ -33,7 +33,7 @@
                             <li class="nav-item" role="presentation">
                             <li class="nav-item" role="presentation">  <a class="nav-link list-inline-item active">Home</a> </li>
                             <li class="nav-item" role="presentation">  <a class="nav-link list-inline-item " href="#section2">About us</a></li>
-                            <li class="nav-item" role="presentation">  <a class="nav-link list-inline-item">Faq</a></li>
+                            <li class="nav-item" role="presentation">  <a class="nav-link list-inline-item" data-toggle="modal" data-target="#myModalFAQ">Faq</a></li>
                             <li class="nav-item" role="presentation">  <a class="nav-link list-inline-item" href="#section3">Services</a></li>
                             <li class="nav-item" role="presentation">  <a class="nav-link list-inline-item" data-toggle="modal" data-target="#myModalContact">Contact us / Opportunities </a></li>
                             </li>
@@ -130,6 +130,67 @@
                                     <div class="modal-footer d-flex justify-content-center">  
                                         <button href="" class="btn btn-outline-success">Submit</button>
                                     </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <!-- Modal FAQ -->
+                    
+             <div class="modal fade" id="myModalFAQ"  tabindex="-1" role="dialog">
+             <div class="modal-dialog modal-md">
+                            <div class="modal-content ">
+                                <div class="modal-header">
+                                    <h5 class="h3 text-dark d-flex justify-content-center" id="myModalLabel">Frequently Ask Question</h5>
+                                    <button class="close" data-dismiss="modal">&times;</button>
+                                </div>
+                                <div class="modal-body">
+                                   <div id="accordion">
+                                        <div class="card">
+                                            <div class="card-header" id="headingOne">
+                                            <h5 class="mb-0">
+                                                <button class="btn btn-link" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                                                What is Green Clean?
+                                                </button>
+                                            </h5>
+                                            </div>
+
+                                            <div id="collapseOne" class="collapse" aria-labelledby="headingOne" data-parent="#accordion">
+                                            <div class="card-body text-dark">
+                                                Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+                                            </div>
+                                            </div>
+                                        </div>
+                                        <div class="card">
+                                            <div class="card-header" id="headingTwo">
+                                            <h5 class="mb-0">
+                                                <button class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                                                 How can I avail your service?
+                                                </button>
+                                            </h5>
+                                            </div>
+                                            <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordion">
+                                            <div class="card-body text-dark">
+                                                Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+                                            </div>
+                                            </div>
+                                        </div>
+                                        <div class="card">
+                                            <div class="card-header" id="headingThree">
+                                            <h5 class="mb-0">
+                                                <button class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                                                Do you have a mobile app? 
+                                                </button>
+                                            </h5>
+                                            </div>
+                                            <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordion">
+                                            <div class="card-body text-dark">
+                                                Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+                                            </div>
+                                            </div>
+                                        </div>
+                                        </div>
+<!-- MODALFAQ-->
                                 </div>
                             </div>
                         </div>
