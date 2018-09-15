972882a3f054bacebec495ee69df2747a0f402ef
Timothy Llanera <llaneratizam@gmail.com>
2018-09-15 18:30:46 +0800

    syntax fix

--- application/models/AppointmentModel.php
+++ application/models/AppointmentModel.php
@@ -1,9 +1,9 @@
 <?php
 class AppointmentModel extends CI_Model{
     public function close_appointment($drop_code, $sender){
-      $select_housekeeper_by_number = "SELECT * FROM housekeeper WHERE contact_number = ?"
-      $verify_appointment_code      = "SELECT * FROM appointments WHERE drop_code = ? AND housekeeper_id = ?"
-      $update_appointment           = "UPDATE appointments SET drop_code = NULL, dropped_by = ?, is_finished = ?, WHERE transaction_id = ?"
+      $select_housekeeper_by_number = "SELECT * FROM housekeeper WHERE contact_number = ?";
+      $verify_appointment_code      = "SELECT * FROM appointments WHERE drop_code = ? AND housekeeper_id = ?";
+      $update_appointment           = "UPDATE appointments SET drop_code = NULL, dropped_by = ?, is_finished = ?, WHERE transaction_id = ?";
       $select_housekeepers_in_appointment = "SELECT contact_number, access_token FROM housekeeper WHERE housekeeper_id IN (SELECT housekeeper_id FROM service_cleaning WHERE transaction_id = ?)";
       $this->db->trans_begin();
       $housekeeper_trigger = $this->db->query($select_housekeeper_by_number, array($sender))->result();
