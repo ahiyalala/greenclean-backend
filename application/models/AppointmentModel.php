1556b1d66174dc05e2e4ce1a2238a9f3e7705e4d
Timothy Llanera <llaneratizam@gmail.com>
2018-09-15 19:30:39 +0800

    logic fix

--- application/models/AppointmentModel.php
+++ application/models/AppointmentModel.php
@@ -6,16 +6,16 @@ class AppointmentModel extends CI_Model{
       $update_appointment           = "UPDATE appointments SET drop_code = NULL, dropped_by = ?, is_finished = ?, WHERE transaction_id = ?";
       $select_housekeepers_in_appointment = "SELECT contact_number, access_token FROM housekeeper WHERE housekeeper_id IN (SELECT housekeeper_id FROM service_cleaning WHERE transaction_id = ?)";
       $this->db->trans_begin();
-      $housekeeper_trigger = $this->db->query($select_housekeeper_by_number, array($sender))->result();
+      $housekeeper_trigger = $this->db->query($select_housekeeper_by_number, array($sender))->row();
       if(!$housekeeper_trigger){
         $this->db->trans_rollback();
         log_message('error','No housekeeper with number '.$sender);
         return false;
       }
       //Check if appointment with code X exists for housekeeper with number X
-      $service_cleaning = $this->db->query($verify_appointment_code, array($drop_code, $housekeeper_trigger->housekeeper_id))->result();
+      $service_cleaning = $this->db->query($verify_appointment_code, array($drop_code, $housekeeper_trigger->housekeeper_id))->row();
       if(!$service_cleaning){
-        $this->db->rollback();
+        $this->db->trans_rollback();
         log_message('error', 'Invalid drop code for '.$housekeeper_trigger->first_name.' '.$housekeeper_trigger->last_name);
         return false;
       }
