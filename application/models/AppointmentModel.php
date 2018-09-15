2717a974ae0611d5a48d47d4fe83c68fca408eb6
Timothy Llanera <llaneratizam@gmail.com>
2018-09-15 20:25:49 +0800

    query and logic fix

--- application/models/AppointmentModel.php
+++ application/models/AppointmentModel.php
@@ -2,7 +2,7 @@
 class AppointmentModel extends CI_Model{
     public function close_appointment($drop_code, $sender){
       $select_housekeeper_by_number = "SELECT * FROM housekeeper WHERE contact_number = ?";
-      $verify_appointment_code      = "SELECT * FROM appointments WHERE drop_code = ? AND housekeeper_id = ?";
+      $verify_appointment_code      = "SELECT * FROM appointments WHERE drop_code = ? AND housekeeper_id = ? AND is_finished = 0";
       $update_service_cleaning      = "UPDATE service_cleaning SET drop_code = NULL, dropped_by = ? WHERE transaction_id = ?";
       $update_payment_transaction   = "UPDATE payment_transaction SET is_finished = ? WHERE transaction_id = ?";
       $select_housekeepers_in_appointment = "SELECT contact_number, globe_access_token FROM housekeeper WHERE housekeeper_id IN (SELECT housekeeper_id FROM service_cleaning WHERE transaction_id = ?)";
@@ -25,13 +25,13 @@ class AppointmentModel extends CI_Model{
       //Update transaction
       $this->db->query($update_service_cleaning, array($housekeeper_trigger->housekeeper_id, $transaction_id));
       $this->db->query($update_payment_transaction, array(1,$transaction_id));
-      $housekeepers = $this->db->query($select_housekeepers_in_appointment, array($transaction_id));
+      $housekeepers = $this->db->query($select_housekeepers_in_appointment, array($transaction_id))->result();
       if($this->db->trans_status()){
         $this->db->trans_rollback();
         $this->load->helper('sms_helper');
         $message = '['.$transaction_id.']'. $housekeeper_trigger->first_name.' '.$housekeeper_trigger->last_name.'['.$housekeeper_trigger->contact_number.'] has marked an appointment as CLOSED.';
-        foreach($housekeepers AS $housekeeper){
-          send_general_message($housekeeper->contact_number, $housekeeper->access_token, $message);
+        foreach($housekeepers as $housekeeper){
+          send_general_message($housekeeper->contact_number, $housekeeper->globe_access_token, $message);
         }
         log_message('info',$message);
         return true;
