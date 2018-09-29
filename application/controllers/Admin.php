42c58d8f72a99fa74f683e30a328ead467fbb314
Timothy Llanera <llaneratizam@gmail.com>
2018-09-29 16:54:06 +0800

    Appointments fix

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -147,7 +147,7 @@ class Admin extends CI_Controller {
         $query = 'select t.is_finished, b.service_type_key, h.first_name as h_first_name, h.last_name as h_last_name, c.first_name as c_first_name, c.last_name as c_last_name, hs.date, hs.start_time, hs.end_time
         from housekeeper_schedule as hs
         left join housekeeper as h on h.housekeeper_id=hs.housekeeper_id
-        left join booking_request as b on b.schedule_id=hs.schedule_id
+        left join booking_request as b on b.booking_request_id=hs.booking_request_id
         left join customer as c on c.customer_id=b.customer_id
         left join payment_transaction as t on t.booking_request_id = b.booking_request_id
         where hs.availability <> 0
