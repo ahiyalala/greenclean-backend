70b55a5b1f0af64937f1846fe51bb4a3e832f20c
Timothy Llanera <llaneratizam@gmail.com>
2018-09-30 00:04:52 +0800

    debugger

--- application/controllers/Feedback.php
+++ application/controllers/Feedback.php
@@ -84,11 +84,11 @@ class Feedback extends Api_Controller{
     if($this->db->trans_status() && $affected_appointment){
       $check_update_query = "SELECT * FROM service_cleaning AS s INNER JOIN payment_transaction AS p ON p.transaction_id = s.transaction_id INNER JOIN housekeeper AS h ON h.housekeeper_id = s.housekeeper_id WHERE service_cleaning_id = ?";
       $check_update = $this->db->query($check_update_query, array($post_data['service_cleaning_id']));
-      log_message('debug',json_encode($check_update->row()));
       $this->db->trans_commit();
       return $this->output->set_status_header(200);
     }
     else{
+      log_message('debug',$affected_appointment.' Error: '.$this->db->error());
       $this->db->trans_rollback();
       return $this->output->set_status_header(500);
     }
